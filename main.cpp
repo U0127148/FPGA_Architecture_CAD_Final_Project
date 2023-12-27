dfedc73b64eb0b3f5a266c799b5c87ccf6fe2c05
eunice012716 <67517541+eunice012716@users.noreply.github.com>
2023-12-27 23:50:02 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@ int main(int argc, char *argv[]) {
     srand(0);
     Solver* solver = new Solver();
     solver->read(argv);
-    solver->init_pop();
+    solver->create_grid();
+    solver->init_pop_from_GP();
+    solver->output_file(argv[4]);
     solver->genetic_algorithm();
     solver->output_file(argv[4]);
     return 0;
